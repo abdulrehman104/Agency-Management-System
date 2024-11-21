7dc109689171dc8b4288571543b9884a265448de
abdulrehman1004 <rehman.abdul008844@gmail.com>
2024-11-21 21:14:26 +0500

    Agency Management System

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -12,13 +12,18 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { pricingCards } from "@/lib/constants";
+import { stripe } from "@/lib/stripe";
 
 export default async function Home() {
+  const prices = await stripe.prices.list({
+    product: process.env.NEXT_PLURA_PRODUCT_ID,
+    active: true,
+  });
+
   return (
     <>
       {/* ========== Header Section  ========== */}
       <section className="h-full w-full flex flex-col items-center justify-center mt-[-50px] md:mt-0 md:pt-[500px] relative">
-
         {/* ========== Site Image grid  ========== */}
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10" />
 
