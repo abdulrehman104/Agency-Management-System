e1ad89cc218469159eacb998c664a3ac6741b40f
abdulrehman1004 <rehman.abdul008844@gmail.com>
2024-12-01 14:42:23 +0500

    Final Production Code

--- src/components/global/blur-page.tsx
+++ src/components/global/blur-page.tsx
@@ -7,7 +7,8 @@ type Props = {
 export const BlurPage = ({ children }: Props) => {
   return (
     <div
-      className="h-screen overflow-scroll backdrop-blur-[35px] dark:bg-muted/40 bg-muted/60 dark:shadow-2xl dark:shadow-black  mx-auto pt-24 p-4 absolute top-0 right-0 left-0 botton-0 z-[11]"
+      // overflow-scroll
+      className="h-screen  backdrop-blur-[35px] dark:bg-muted/40 bg-muted/60 dark:shadow-2xl dark:shadow-black  mx-auto pt-24 p-4 absolute top-0 right-0 left-0 botton-0 z-[11]"
       id="blur-page"
     >
       {children}
